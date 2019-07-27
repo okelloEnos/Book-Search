b1c38e02d91335591bd7c0262f84e33ce1b3b279
okelloEnos <enosokello@gmail.com>
2019-07-27 16:30:38 +0300

    First Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Book-Search-App
+An App to Search for on-line books
+Using the Google Books Api in Implementation
+You can search for any book related to your description
---
+++ app/src/main/java/com/okellosoftwarez/booklistactivity/Book.java
@@ -0,0 +1,63 @@
+package com.okellosoftwarez.booklistactivity;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.text.TextUtils;
+
+public class Book implements Parcelable {
+    public String id;
+    public String title;
+    public String subTitle;
+    public String authors;
+    public String publisher;
+    public String publishDate;
+    public String description;
+
+    public Book(String id, String title, String subTitle, String[] authors, String publisher, String publishDate, String description) {
+        this.id = id;
+        this.title = title;
+        this.subTitle = subTitle;
+        this.authors = TextUtils.join(" ,",authors);
+        this.publisher = publisher;
+        this.publishDate = publishDate;
+        this.description = description;
+    }
+
+    protected Book(Parcel in) {
+        id = in.readString();
+        title = in.readString();
+        subTitle = in.readString();
+        authors = in.readString();
+        publisher = in.readString();
+        publishDate = in.readString();
+        description = in.readString();
+    }
+
+    public static final Creator<Book> CREATOR = new Creator<Book>() {
+        @Override
+        public Book createFromParcel(Parcel in) {
+            return new Book(in);
+        }
+
+        @Override
+        public Book[] newArray(int size) {
+            return new Book[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(id);
+        parcel.writeString(title);
+        parcel.writeString(subTitle);
+        parcel.writeString(authors);
+        parcel.writeString(publisher);
+        parcel.writeString(publishDate);
+        parcel.writeString(description);
+    }
+}
